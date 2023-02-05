2304b6a54ef8e31278aa12824e6a61dec4b6facc
LKS <kyungseolee0815@gmail.com>
2023-02-05 17:54:03 +0900

    modified : return type

--- Algorithm/j.cpp
+++ Algorithm/j.cpp
@@ -16,11 +16,31 @@ double d(){
 	return 1.245;
 }
 
+void forLoop(){
+	string s[2] = {"out of time", "I love you"};
+	for(string b : s) cout << b << "\n"; // these two are same
+	for(int i = 0; i < 2; i++) cout << s[i] << "\n";
+} 
+
+bool trueOrFalse(){
+	// 0 is false, else is true
+	int a = -1;
+	cout << bool(a) << "\n";
+	a = 0;
+	cout << bool(a) << "\n";
+	a = 3;
+	cout << bool(a) << "\n";
+	
+	return a;
+}
+
 int main(){
 	v();
 	double answer = d();
 	cout << answer << "\n";
 	cout << c() << "\n";
+	forLoop();
+	trueOrFalse();
 	
 	return 0;
 }
